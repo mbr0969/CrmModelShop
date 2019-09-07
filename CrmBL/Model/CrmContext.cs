5ec18db66095510ec99cd8f42147167ffe0b6fc0
mbr0969 <mbr0969@gmail.com>
2019-09-07 22:00:04 +0300

    07.09.2019

---
+++ CrmBL/Model/CrmContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CrmBL.Model {
+    public class CrmContext : DbContext {
+
+        public CrmContext() :base("CRMConnection") {}
+
+        public DbSet<Check> Checks { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sell> Sells { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+
+    }
+}
---
+++ CrmUI/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+using CrmBL.Model;
+
+namespace CrmUI {
+    public partial class Main : Form {
+
+        CrmContext  db;
+
+        public Main() {
+            InitializeComponent();
+            db = new CrmContext();
+        }
+        private void ProductToolStripMenuItem_Click(object sender, EventArgs e) {
+          
+        }
+
+        private void SellerToolStripMenuItem_Click(object sender, EventArgs e) {
+            var catalogSeller = new Catalog<Seller>(db.Sellers);
+            catalogSeller.Show();
+        }
+
+        private void CustomerToolStripMenuItem_Click(object sender, EventArgs e) {
+            var catalogCustomer = new Catalog<Customer>(db.Customers);
+            catalogCustomer.Show();
+        }
+
+        private void CheckToolStripMenuItem_Click(object sender, EventArgs e) {
+
+            var checkCustomer = new Catalog<Check>(db.Checks);
+            checkCustomer.Show();
+
+        }
+
+        private void CustomerAddToolStripMenuItem2_Click(object sender, EventArgs e) {
+
+            var form = new CustomerForm();
+
+            if (form.ShowDialog() == DialogResult.OK){
+                db.Customers.Add(form.Customer);
+                db.SaveChanges();
+            }
+        }
+    }
+}
